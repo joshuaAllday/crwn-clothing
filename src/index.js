a2255b3903929c8d57828d46cd99f92f672d05d7
Joshua Allday <joshuaallday@Joshuas-Air-2.lan>
2019-07-19 15:54:10 +0100

    added redux to the app and implemented user actions and user reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
 ReactDOM.render(
-	<Provider>
+	<Provider store = {store} >
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
