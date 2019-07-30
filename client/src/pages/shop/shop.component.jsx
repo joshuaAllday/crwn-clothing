2d13c742b03ef60423d43bba15f0aa965bbff895
Joshua Allday <joshallday@icloud.com>
2019-07-30 14:20:47 +0100

    implemented lazy loading and suspense into the app

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionPageContainer from '../collection/collection.container';
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import Spinner from '../../components/spinner/spinner.component';
+
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'))
+const CollectionPageContainer =lazy(() => import('../collection/collection.container'))
 
 class ShopPage extends React.Component {
 
@@ -18,9 +20,11 @@ class ShopPage extends React.Component {
 		const { match } = this.props;
 
 		return(
-			<div className='shop-page'>  
-				<Route exact path={`${match.path}`} component={ CollectionsOverviewContainer } />
-				<Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+			<div className='shop-page'>
+				<Suspense fallback={<Spinner />}>
+					<Route exact path={`${match.path}`} component={ CollectionsOverviewContainer } />
+					<Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+				</Suspense>
 			</div>
 		);
 	};
